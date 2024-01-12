e940848fcbf7212de9aa141cee7d1204ad6c1b5c
brandon-kong <kongbrandon0@gmail.com>
2024-01-12 11:45:45 -0600

    Added User model with managers, forms, admin

--- core/user/admin.py
+++ core/user/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UserAdminModule
 
-# Register your models here.
+from .models import User
+from .forms import UserCreationForm, UserChangeForm
+
+class UserAdmin(UserAdminModule):
+    """
+    Custom User Admin module
+    """
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    fields = None
+    list_display = ["email", "first_name", "last_name", "is_staff"]
+
+    ordering = ["email"]
+
+admin.site.register(User, UserAdmin)
+
--- core/user/models.py
+++ core/user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import UserManager
+
+class User(AbstractUser):
+    """
+    User model for authentication
+    """
+    
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    first_name = models.CharField(max_length=64, blank=False)
+    last_name = models.CharField(max_length=64, blank=False)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
